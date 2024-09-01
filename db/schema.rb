ea96aa8754195886a8652324684998bebb40a372
caiodsc <caio.dscamara@gmail.com>
2024-08-31 21:53:44 -0400

    feature: add steady to boards

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_240_831_164_308) do
+ActiveRecord::Schema[7.1].define(version: 20_240_831_221_542) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -22,5 +22,6 @@ ActiveRecord::Schema[7.1].define(version: 20_240_831_164_308) do
     t.text 'grid', null: false
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+    t.boolean 'steady', default: false, null: false
   end
 end
